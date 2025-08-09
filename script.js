486e64c6fdb2be05193519cca2be166780c454c1
gitmcy0131 <chunyan.mo@edu.sait.ca>
2025-08-09 01:35:05 -0600

    commit-8.8

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 document.getElementById("menu-toggle").addEventListener("click", function () {
     document.getElementById("nav-links").classList.toggle("active");
 });
+
+/* addEventListener to change background color */
+const changeBgBtn = document.getElementById("changeBgBtn");
+const aboutSection = document.getElementById("about");
+
+const originalColor = "#ffffff";
+const newColor = "#3ae8f4ff";
+
+aboutSection.style.backgroundColor = originalColor;
+
+changeBgBtn.addEventListener("click", function () {
+    const currentColor = window.getComputedStyle(aboutSection).backgroundColor;
+
+    if (currentColor === "rgb(255, 255, 255)") {
+        aboutSection.style.backgroundColor = newColor;
+    } else {
+        aboutSection.style.backgroundColor = originalColor;
+    }
+
+    console.log("Button clicked! Background color toggled.");
+});
